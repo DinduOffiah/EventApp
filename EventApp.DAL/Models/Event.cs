a65d2e7ccf2a9aebc52cae341f76ec34d7b8589e
Chidindu <chidinduoffiah@gmail.com>
2024-04-18 11:40:29 +0100

    Added Startdate, Enddate, and Description to Event Model

--- EventApp.DAL/Models/Event.cs
+++ EventApp.DAL/Models/Event.cs
@@ -6,11 +6,14 @@
         public string? EventName { get; set; }
         public byte[]? Image { get; set; }
         public DateTime? EventDate { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime EndDate { get; set; }
         public string? Location { get; set;}
         public int? Limit { get; set; }
         public int TicketTypeId { get; set; }
         public TicketType? TicketType { get; set; }
         public int EventTypeId { get; set; }
+        public string? Description { get; set; }
         public EventType? EventType { get; set; }
         public DateTime DateCreated { get; set; } = DateTime.Now;
         public DateTime LastModeifed { get; set; } = DateTime.Now;
